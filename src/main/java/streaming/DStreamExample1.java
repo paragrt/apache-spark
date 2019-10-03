4127963fe9b6b555ed033d80b6d7c960298e2b0b
Parag Teredesai <paragrt@hotmail.com>
2019-10-02 23:18:11 -0500

    added spark streaming

---
+++ src/main/java/streaming/DStreamExample1.java
@@ -0,0 +1,41 @@
+package streaming;
+
+import java.util.Arrays;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+
+import scala.Tuple2;
+
+public class DStreamExample1 {
+	
+	public static void main(String[] args) throws InterruptedException {
+		Logger.getLogger("org.apache").setLevel(Level.WARN);
+		SparkConf conf = new SparkConf().
+				setMaster("local[2]").setAppName("NetworkWordCount");
+		JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1) );
+		
+		// Create a DStream that will connect to hostname:port, like localhost:9999
+		JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
+		
+		// Split each line into words
+		JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
+		// Count each word in each batch
+		JavaPairDStream<String, Integer> pairs = words.mapToPair(s -> new Tuple2<>(s, 1));
+		JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey((i1, i2) -> i1 + i2);
+
+		// Print the first ten elements of each RDD generated in this DStream to the console
+		wordCounts.print();
+		
+		jssc.start();
+		jssc.awaitTermination();
+		
+	}
+
+}
